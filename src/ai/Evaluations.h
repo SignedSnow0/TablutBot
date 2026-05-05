6da4f201a7e1a9f03776f85c0a555223e4ade2ad
lorenzoarlo <arlotti.lorenzo2@gmail.com>
2026-05-05 09:48:21 +0200

    Added comments descriptions about evaluations functions

--- src/ai/Evaluations.h
+++ src/ai/Evaluations.h
@@ -3,12 +3,46 @@
 
 #include "state/Tablut.h"
 
+/*
+ * Calculates the total number of unobstructed, straight-line paths the King
+ * currently has to the edges of the board.
+ */
 uint8_t EscapeRoutes(const Tablut &t, const PiecePosition &king);
+
+/**
+ * Counts how many enemy Mercenaries (Black pieces) are in the immediate
+ * vicinity (3 x 3 grid centered in the king) of the King.
+ */
 uint8_t MercenariesAdjacentKing(const Tablut &t, const PiecePosition &king);
+
+/**
+ * Counts how many Guards (White pieces) are in the immediate vicinity (3 x 3
+ * grid centered in the king) of the King.
+ */
 uint8_t GuardsAdjacentKing(const Tablut &t, const PiecePosition &king);
 
+/**
+ * Calculates the minimum number of moves required for a piece (usually the
+ * King) to escape to any edge of the board.
+ * @returns The number of sequential moves to reach the edge, or 255 (the
+ * maximum value for a uint8_t) if the piece is completely trapped.
+ */
 uint8_t BfsDistanceToEdge(const Tablut &t, const PiecePosition &p);
 
+/**
+ * Evaluates the overall positional strength of the White pieces (Guards)
+ * It uses a weighted sum of the positions of the pieces
+ */
 int64_t PositionWeigthedWhite(const Tablut &t);
+
+/**
+ * Evaluates the overall positional strength of the King
+ * It uses a weighted sum of the positions of the pieces
+ */
 int64_t PositionWeigthedKing(const Tablut &t);
+
+/**
+ * Evaluates the overall positional strength of the Black pieces (Mercenaries)
+ * It uses a weighted sum of the positions of the pieces
+ */
 int64_t PositionWeigthedBlack(const Tablut &t);
